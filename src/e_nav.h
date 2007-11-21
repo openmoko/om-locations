c856c43bbaf5bc5f2dc3432e0d51f6ee9e37093c
raster <raster@66881bcf-9a29-0410-b710-b4726655af6a>
2007-11-21 11:40:40 +0000

    proto--
    git-svn-id: svn+ssh://svn.projects.openmoko.org/svnroot/diversity/trunk/diversity-nav@158 66881bcf-9a29-0410-b710-b4726655af6a

--- src/e_nav.h
+++ src/e_nav.h
@@ -16,12 +16,6 @@ typedef enum _E_Nav_World_Item_Type
 Evas_Object    *e_nav_add(Evas *e);
 void            e_nav_theme_source_set(Evas_Object *obj, const char *custom_dir);
     
-/* location stack */
-E_Nav_Location *e_nav_location_push(Evas_Object *obj);
-void            e_nav_location_pop(Evas_Object *obj);
-void            e_nav_location_del(Evas_Object *obj, E_Nav_Location *loc);
-E_Nav_Location *e_nav_location_get(Evas_Object *obj);
-   
 /* spatial & zoom controls */
 void            e_nav_coord_set(Evas_Object *obj, double lat, double lon, double when);
 double          e_nav_coord_lat_get(Evas_Object *obj);
