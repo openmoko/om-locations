5d88ee1cfac6336e5ed6e4bbad90d471501aab52
olvaffe <olvaffe@66881bcf-9a29-0410-b710-b4726655af6a>
2008-03-12 11:50:27 +0000

    remove test code
    git-svn-id: svn+ssh://svn.projects.openmoko.org/svnroot/diversity/trunk/diversity-nav@245 66881bcf-9a29-0410-b710-b4726655af6a

--- src/e_mod_nav.c
+++ src/e_mod_nav.c
@@ -49,100 +49,6 @@ static Diversity_World *world = NULL;
 static Diversity_Bard  *self  = NULL;
 static Diversity_Viewport *worldview = NULL;
 
-static void add_city(Evas* evas, double lon, double lat, const char* cityname);
-static void test_map(Evas* evas);
-
-static Evas_Object *
-theme_obj_new(Evas *e, const char *custom_dir, const char *group)
-{
-   Evas_Object *o;
-   
-   o = edje_object_add(e);
-   if (!e_nav_edje_object_set(o, "default", group))
-     {
-	if (custom_dir)
-	  {
-	     char buf[PATH_MAX];
-	     
-	     snprintf(buf, sizeof(buf), "%s/default.edj", custom_dir);
-	     edje_object_file_set(o, buf, group);
-	  }
-     }
-   return o;
-}
-
-static void
-city_menu_cb_1(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb1\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_menu_cb_2(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb2\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_menu_cb_3(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb3\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_menu_cb_4(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb4\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_menu_cb_5(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb5\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_menu_cb_6(void *data, Evas_Object *obj, Evas_Object *src_obj)
-{
-   printf("cb6\n");
-   e_spiralmenu_deactivate(obj);
-}
-
-static void
-city_mouse_cb_down(void *data, Evas *evas, Evas_Object *obj, void *event)
-{
-   Evas_Object *om;
-   
-   om = e_spiralmenu_add(evas);
-   e_spiralmenu_theme_source_set(om, data);
-   e_spiralmenu_autodelete_set(om, 1);
-   e_spiralmenu_deacdelete_set(om, 1);
-   e_spiralmenu_source_object_set(om, obj);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Silly", city_menu_cb_1, NULL);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Fish", city_menu_cb_2, NULL);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Onion", city_menu_cb_3, NULL);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Eggplant", city_menu_cb_4, NULL);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Potatoe", city_menu_cb_5, NULL);
-   e_spiralmenu_theme_item_add(om, "modules/diversity_nav/item", 48, "Plum", city_menu_cb_6, NULL);
-   evas_object_show(om);
-   e_spiralmenu_activate(om);
-   printf("menu activate for %p = %p\n", obj, om);
-}
-
-static void
-map_resize(void *data, Evas *evas, Evas_Object *obj, void *event_info)
-{
-   Evas_Coord w, h;
-   
-   evas_object_geometry_get(obj, NULL, NULL, &w, &h);
-   evas_object_image_fill_set(obj, 0, 0, w, h);
-}
-
 static Evas_Object *
 osm_tileset_add(Evas_Object *nav)
 {
@@ -183,6 +89,51 @@ osm_tileset_add(Evas_Object *nav)
    return nt;
 }
 
+static void
+viewport_object_added(void *data, DBusMessage *msg)
+{
+   const char *obj_path;
+   DBusError error;
+   dbus_error_init(&error);
+   if (!dbus_message_get_args(msg, &error,
+			      DBUS_TYPE_OBJECT_PATH, &obj_path,
+			      DBUS_TYPE_INVALID))
+     {
+        printf("object added parse error: %s\n", error.message);
+	dbus_error_free(&error);
+        return;
+     }
+   else 
+     {
+        Diversity_Object *obj;
+        double lon, lat, width, height;
+	int type;
+
+        printf("object added in the viewport, path:%s\n", obj_path);  
+	obj = diversity_object_new(obj_path);
+	if (!obj)
+	  return;
+
+        diversity_object_geometry_get(obj, &lon, &lat, &width, &height);
+        printf("location geo get lon:%f lat:%f\n", lon, lat);
+       	type = diversity_object_type_get(obj);  
+        if(type==DIVERSITY_OBJECT_TYPE_TAG) 
+          {
+             Evas_Object *loc_obj = e_nav_world_item_location_add(nav, THEME_PATH,
+				     lon, lat, obj);
+             char *name = NULL;
+             char *description = NULL;
+             diversity_tag_prop_get((Diversity_Tag *) obj, "description", &description); 
+             name = strsep(&description, "\n");
+             e_nav_world_item_location_name_set(loc_obj, name);
+             e_nav_world_item_location_note_set(loc_obj, description);
+             e_ctrl_taglist_tag_add(name, description, loc_obj); 
+          }
+        else
+          printf("other kind of object added\n");
+     }
+}
+
 void
 _e_mod_nav_init(Evas *evas)
 {
@@ -204,79 +155,18 @@ _e_mod_nav_init(Evas *evas)
    e_ctrl_nav_set(nav);
    evas_object_show(ctrl);
 
-   /* testing items */
-   test_map(evas); 
-   nwi = theme_obj_new(evas, THEME_PATH, "modules/diversity_nav/city");
-   evas_object_event_callback_add(nwi, EVAS_CALLBACK_MOUSE_DOWN,
-				  city_mouse_cb_down,
-				  THEME_PATH);
-   edje_object_part_text_set(nwi, "e.text.name", "Sydney");
-   e_nav_world_item_add(nav, nwi);
-   e_nav_world_item_type_set(nwi, E_NAV_WORLD_ITEM_TYPE_ITEM);
-   e_nav_world_item_geometry_set(nwi,
-				 151.205907, 33.875938,
-				 32, 32);
-   e_nav_world_item_scale_set(nwi, 0);
-   e_nav_world_item_update(nwi);
-   evas_object_show(nwi);
-   
-   nwi = theme_obj_new(evas, THEME_PATH,
-		       "modules/diversity_nav/city");
-   evas_object_event_callback_add(nwi, EVAS_CALLBACK_MOUSE_DOWN,
-				  city_mouse_cb_down,
-				  THEME_PATH);
-   edje_object_part_text_set(nwi, "e.text.name", "Taipei");
-   e_nav_world_item_add(nav, nwi);
-   e_nav_world_item_type_set(nwi, E_NAV_WORLD_ITEM_TYPE_ITEM);
-   e_nav_world_item_geometry_set(nwi,
-				 121.549644, -25.046414,
-				 32, 32);
-   e_nav_world_item_scale_set(nwi, 0);
-   e_nav_world_item_update(nwi);
-   evas_object_show(nwi);
-   
-   nwi = theme_obj_new(evas, THEME_PATH,
-		       "modules/diversity_nav/city");
-   evas_object_event_callback_add(nwi, EVAS_CALLBACK_MOUSE_DOWN,
-				  city_mouse_cb_down,
-				  THEME_PATH);
-   edje_object_part_text_set(nwi, "e.text.name", "Kings Cross");
-   e_nav_world_item_add(nav, nwi);
-   e_nav_world_item_type_set(nwi, E_NAV_WORLD_ITEM_TYPE_ITEM);
-   e_nav_world_item_geometry_set(nwi, 
-				 151.223588, 33.873622,
-				 32, 32);
-   e_nav_world_item_scale_set(nwi, 0);
-   e_nav_world_item_update(nwi);
-   evas_object_show(nwi);
-   
-   nwi = evas_object_image_add(evas);
-   evas_object_image_file_set(nwi, "/tmp/sydney_city_map.png", NULL);
-   evas_object_image_smooth_scale_set(nwi, 0);
-   evas_object_event_callback_add(nwi, EVAS_CALLBACK_RESIZE, map_resize, NULL);
-   e_nav_world_item_add(nav, nwi);
-   e_nav_world_item_type_set(nwi, E_NAV_WORLD_ITEM_TYPE_WALLPAPER);
-   e_nav_world_item_geometry_set(nwi, 
-				 151.205907, 33.875938, 
-				 2 * (151.232171 - 151.205907), 
-				 (928 * 2 * (151.232171 - 151.205907)) / 1606);
-   e_nav_world_item_scale_set(nwi, 1);
-   e_nav_world_item_update(nwi);
-   evas_object_show(nwi);
+   if(world) 
+     {
+	worldview = diversity_world_viewport_add(world, -180, 90, 180, -90); // whole world viewport
+	diversity_dbus_signal_connect((Diversity_DBus *) worldview, 
+                                      DIVERSITY_DBUS_IFACE_VIEWPORT, 
+                                      "ObjectAdded", 
+                                      viewport_object_added,
+                                      NULL); 
+	printf("Create viewport for whole world\n");
+	diversity_viewport_start(worldview);
+     }
 
-   nwi = e_nav_world_item_ap_add(nav, THEME_PATH,
-				 151.220000, 33.874000);
-   e_nav_world_item_ap_essid_set(nwi, "OpenMoko");
-   e_nav_world_item_ap_key_type_set(nwi, E_NAV_ITEM_AP_KEY_TYPE_NONE);
-   e_nav_world_item_ap_range_set(nwi, 100 NAV_UNIT_M);
-    
-   nwi = e_nav_world_item_neo_other_add(nav, THEME_PATH,
-				     151.215000, 33.871000);
-   e_nav_world_item_neo_other_name_set(nwi, "Sean");
-   nwi = e_nav_world_item_neo_other_add(nav, THEME_PATH,
-				     151.213000, 33.874000);
-   e_nav_world_item_neo_other_name_set(nwi, "Olv");
-   
    /* test NEO ME object */
    nwi = e_nav_world_item_neo_me_add(nav, THEME_PATH,
 				     151.210000, 33.870000);
@@ -327,87 +217,4 @@ _e_mod_nav_shutdown(void)
 
    evas_object_del(nav);
    nav = NULL;
-}
-
-static void
-viewport_object_added(void *data, DBusMessage *msg)
-{
-   const char *obj_path;
-   DBusError error;
-   dbus_error_init(&error);
-   if (!dbus_message_get_args(msg, &error,
-			      DBUS_TYPE_OBJECT_PATH, &obj_path,
-			      DBUS_TYPE_INVALID))
-     {
-        printf("object added parse error: %s\n", error.message);
-	dbus_error_free(&error);
-        return;
-     }
-   else 
-     {
-        Diversity_Object *obj;
-        double lon, lat, width, height;
-	int type;
-
-        printf("object added in the viewport, path:%s\n", obj_path);  
-	obj = diversity_object_new(obj_path);
-	if (!obj)
-	  return;
-
-        diversity_object_geometry_get(obj, &lon, &lat, &width, &height);
-        printf("location geo get lon:%f lat:%f\n", lon, lat);
-       	type = diversity_object_type_get(obj);  
-        if(type==DIVERSITY_OBJECT_TYPE_TAG) 
-          {
-             Evas_Object *loc_obj = e_nav_world_item_location_add(nav, THEME_PATH,
-				     lon, lat, obj);
-             char *name = NULL;
-             char *description = NULL;
-             diversity_tag_prop_get((Diversity_Tag *) obj, "description", &description); 
-             name = strsep(&description, "\n");
-             e_nav_world_item_location_name_set(loc_obj, name);
-             e_nav_world_item_location_note_set(loc_obj, description);
-             e_ctrl_taglist_tag_add(name, description, loc_obj); 
-          }
-        else
-          printf("other kind of object added\n");
-     }
-}
-
-// for test
-static void test_map(Evas* evas)
-{
-    add_city(evas, -0.087891, -51.495065, "London");
-    add_city(evas, 33.178711, -68.989925, "Mypmahck");
-    add_city(evas, 116.28, -39.54, "Beijing");
-    add_city(evas, -122.32974, -47.6035, "Seattle");
-    // create a viewport
-    if(world) 
-      {
-         worldview = diversity_world_viewport_add(world, -180, 90, 180, -90); // whole world viewport
-         diversity_dbus_signal_connect((Diversity_DBus *) worldview, 
-                                       DIVERSITY_DBUS_IFACE_VIEWPORT, 
-                                       "ObjectAdded", 
-                                       viewport_object_added,
-                                       NULL); 
-         printf("Create viewport for whole world\n");
-         diversity_viewport_start(worldview);
-      }
-}
-
-static void add_city(Evas* evas, double lon, double lat, const char* cityname)
-{
-  Evas_Object*  nwi = theme_obj_new(evas, THEME_PATH, "modules/diversity_nav/city");
-   evas_object_event_callback_add(nwi, EVAS_CALLBACK_MOUSE_DOWN,
-                                  city_mouse_cb_down,
-                                  THEME_PATH);
-   edje_object_part_text_set(nwi, "e.text.name", cityname);
-   e_nav_world_item_add(nav, nwi);
-   e_nav_world_item_type_set(nwi, E_NAV_WORLD_ITEM_TYPE_ITEM);
-   e_nav_world_item_geometry_set(nwi,
-                                 lon, lat,
-                                 32, 32);
-   e_nav_world_item_scale_set(nwi, 0);
-   e_nav_world_item_update(nwi);
-    evas_object_show(nwi);
 }
