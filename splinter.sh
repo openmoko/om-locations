f7f74de5c0223662406d7d54b7395012264e476d
jeremy_chang <jeremy_chang@66881bcf-9a29-0410-b710-b4726655af6a>
2008-06-21 02:38:48 +0000

    2008-06-21  Jeremy Chang  <jeremy@openmoko.com>
            * Change exec file name and comment to "splinter"
    
    
    
    git-svn-id: svn+ssh://svn.projects.openmoko.org/svnroot/diversity/branches/splinter@433 66881bcf-9a29-0410-b710-b4726655af6a

--- splinter.sh
+++ splinter.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo 1 > /sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0073/neo1973-pm-gps.0/pwron
-diversity-nav
+splinter
 echo 0 > /sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0073/neo1973-pm-gps.0/pwron
