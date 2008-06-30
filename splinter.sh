e3da27b116522a2b9e51e30028eaded2048cb99c
jeremy_chang <jeremy_chang@66881bcf-9a29-0410-b710-b4726655af6a>
2008-06-30 03:30:15 +0000

    2008-06-30  Jeremy Chang  <jeremy@openmoko.com>
    	* Remove nasty script that turns gps on/off. may there's another app need gps.
    
    
    
    git-svn-id: svn+ssh://svn.projects.openmoko.org/svnroot/diversity/branches/splinter@440 66881bcf-9a29-0410-b710-b4726655af6a

--- splinter.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo 1 > /sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0073/neo1973-pm-gps.0/pwron
-splinter
-echo 0 > /sys/devices/platform/s3c2440-i2c/i2c-adapter/i2c-0/0-0073/neo1973-pm-gps.0/pwron
