309998bf2d87e54612e6d57c29c6ccc0081f25c3
Chia-I Wu <olv@openmoko.com>
2008-09-11 17:15:29 +0800

    Added e_nav_theme_component_new.

--- src/e_nav_theme.h
+++ src/e_nav_theme.h
@@ -32,6 +32,7 @@ const char      *e_nav_theme_path_get(void);
 /* custom_dir is ignored */
 Evas_Object     *e_nav_theme_object_new(Evas *e, const char *custom_dir, const char *group);
 int              e_nav_theme_object_set(Evas_Object *o, const char *custom_dir, const char *group);
+Evas_Object     *e_nav_theme_component_new(Evas *e, const char *base_group, const char *comp, int check);
 int              e_nav_theme_group_exist(const char *custom_dir, const char *group);
 
 #endif /* E_NAV_THEME_H */
